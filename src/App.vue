bbf3527d4e801ab7104c992d0e78a00c90e57a8d
Badr Aldugaish <b.aldugaish@jeel.co>
2026-01-08 01:42:33 +0300

    V1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Obituaries from './components/Obituaries.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="MAX RULES!!!!!!!!!!!!!!!!!!!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <Obituaries />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+body {
+  margin: 0;
+  background: #1a1a1a;
 }
 </style>
